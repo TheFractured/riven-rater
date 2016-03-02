c118e9d42ac432c3f1ba956f320e8b02e2a26538
TheFractured <diego_delgado@live.com>
2016-03-02 15:23:19 -0500

    Added files via upload

--- winrate_difference_calculator.py
+++ winrate_difference_calculator.py
@@ -14,18 +14,18 @@ class winrate_calcuations(object):
         winrate_percent_difference = round(winrate_difference / winrate_average * 100, 3)
 
         player_wins = math.floor(player_winrate*10)
-        print player_wins, 'player wins' #For testing
+        #print player_wins, 'player wins'
         player_losses = math.floor(1000 - player_wins)
-        print player_losses, 'player losses' #For testing
+        #print player_losses, 'player losses'
 
         public_wins = math.floor(public_winrate*10)
-        print public_wins, 'public wins' #For testing
+        #print public_wins, 'public wins'
         public_losses = math.floor(1000 - public_wins)
-        print public_losses, 'public losses' #For testing
+        #print public_losses, 'public losses'
 
         oddsratio, pvalue = stats.fisher_exact([[player_wins, player_losses], [public_wins, public_losses]])
-        print 'pvalue =', pvalue
+        #print 'pvalue =', pvalue
         if pvalue <= 0.05:
-            print 'There\'s a statistically significant', winrate_percent_difference, '% difference between', self.player_name,'\'s winrate and the average winrate.'
+            print 'There\'s a statistically significant', winrate_percent_difference, '% difference (', winrate_difference, 'points ) between', self.player_name, '\'s winrate and the average winrate.'
         else:
-            print 'There\'s a statistically insignificant', winrate_percent_difference, '% difference between', self.player_name,'\'s winrate and the average winrate.'
+            print 'There\'s a statistically insignificant', winrate_percent_difference, '% difference  (', winrate_difference, 'points ) between', self.player_name, '\'s winrate and the average winrate.'
